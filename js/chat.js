abb16bdb549f57b0bf10f45ac58ea5e9a49d79e4
icelala <47877399+icelala@users.noreply.github.com>
2019-06-20 13:55:36 +0800

    Add files via upload

--- js/chat.js
+++ js/chat.js
@@ -4,7 +4,7 @@ function chat(e) {
     var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
     var yyyy = today.getFullYear();
 
-    today = mm + '/' + dd + '/' + yyyy;
+    today = mm  + dd  + yyyy;
     if(e.keyCode == 13){
         db.collection('message').doc(today).set({
             name : getCookie("name"),
